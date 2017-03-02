ba6a7df7b2467fea996d6b58fdaf732652139943
HakunaMacouta <thomas.blanc.2@etu.u-bordeaux.fr>
2017-03-02 09:44:20 +0100

    Ajout IHM fonctionnel César

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,7 +1,29 @@
 package sample;
 
+import crypto.Cesar;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+import javax.xml.soap.Text;
+import java.util.Objects;
+
 /**
  * Created by Thomas on 02/03/2017.
  */
 public class Controller {
+    @FXML
+    TextArea cesarInput;
+    @FXML
+    TextArea cesarOutput;
+    @FXML
+    TextField cesarDecalage;
+
+    @FXML public void onCesarClicked() {
+        if(!Objects.equals(cesarInput.getText(), "")) {
+            cesarOutput.setText(Cesar.crypt(cesarInput.getText(),2));
+        }
+
+    }
 }
