963c3329557aaf2003b49b3e061a4efcdc85b5d3
HakunaMacouta <thomas.blanc.2@etu.u-bordeaux.fr>
2017-03-06 08:45:44 +0100

    ajout algo diffieHelman

--- src/crypto/DiffieHellman.java
+++ src/crypto/DiffieHellman.java
@@ -3,32 +3,31 @@ package crypto;
 import java.math.BigInteger;
 import java.security.SecureRandom;
 
-/**
- * Created by Guillaume on 02/03/2017.
- */
 public class DiffieHellman {
-    private static BigInteger p;
-    private static BigInteger g;
-    private static BigInteger a;
-    private static BigInteger b;
-    private static BigInteger gModPowA;
-    private static BigInteger gModPowB;
-    private static BigInteger cle;
-
+    private BigInteger PrimeModulus, generator, alicePrivateNumber, alicePublicNumber, bobPublicNumber, SecretKey;
     private static final int bitLength = 512;
-
-
     public DiffieHellman() {
         SecureRandom rnd = new SecureRandom();
-        p = BigInteger.probablePrime(bitLength, rnd);
-        g = BigInteger.probablePrime(bitLength, rnd);
-        a = new BigInteger("1234567"); //choisi par moi
-    }
-    public String calculateKey(BigInteger b) {
-        gModPowA =  g.modPow(a,p);
-        gModPowB =  new BigInteger("1210796"); //Valeur envoyé par M. Leothaud
-        cle = gModPowB.modPow(a,p);
-        return cle.toString();
+        PrimeModulus = BigInteger.probablePrime(bitLength,rnd);
+        generator = BigInteger.valueOf(rnd.nextLong());
+        alicePrivateNumber = BigInteger.valueOf(rnd.nextLong());
+        alicePublicNumber = PrimeModulus.modPow(alicePrivateNumber,generator);
     }
 
+    public BigInteger getPrimeModulus() {
+        return PrimeModulus;
+    }
+
+    public BigInteger getGenerator() {
+        return generator;
+    }
+
+    public void setBobNumber(BigInteger b) {
+        bobPublicNumber = b;
+    }
+
+    public String calculateKey() {
+        SecretKey = bobPublicNumber.modPow(alicePrivateNumber, generator);
+        return SecretKey.toString();
+    }
 }
